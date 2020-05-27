3d90a29bbbc1793248bac8f67317f8cae3714510
Sam Dixon <sam.dixon@berkeley.edu>
2020-05-26 18:44:05 -0700

    fix filenames

--- gen_sim_scripts.py
+++ gen_sim_scripts.py
@@ -9,7 +9,7 @@ os.makedirs(LOG_DIR, exist_ok=True)
 
 
 TEMPLATE = """#!/bin/bash
-#$ -N {alpha}_{beta}
+#$ -N mc_{alpha}_{beta}
 #$ -e {curr_dir}/logs
 #$ -o {curr_dir}/logs
 /home/samdixon/anaconda3/bin/python {curr_dir}/sims.py --alpha {alpha} --beta {beta}"""
@@ -19,13 +19,12 @@ alphas = np.linspace(0.05, 0.2, 11)
 betas = np.linspace(2.5, 3.5, 11)
 for alpha in alphas:
     for beta in betas:
-        script_fname = '{:0.0f}_{:0.0f}.sh'.format(alpha*100, beta*10)
-        print(script_fname)
-        # script_path = os.path.join(SCRIPT_DIR, script_fname)
-        # with open(script_path, 'w') as f:
-        #     props = {'alpha': alpha,
-        #              'beta': beta,
-        #              'curr_dir': os.curdir}
-        #     f.write(TEMPLATE.format(**props))
-        # with open(SUBMIT_PATH, 'a') as f:
-        #     f.write('qsub {}\n'.format(script_path))
+        script_fname = '{:02.0f}_{:02.0f}.sh'.format(alpha*100, beta*10)
+        script_path = os.path.join(SCRIPT_DIR, script_fname)
+        with open(script_path, 'w') as f:
+            props = {'alpha': np.round(alpha, 2),
+                     'beta': np.round(beta, 1),
+                     'curr_dir': os.curdir}
+            f.write(TEMPLATE.format(**props))
+        with open(SUBMIT_PATH, 'a') as f:
+            f.write('qsub {}\n'.format(script_path))
